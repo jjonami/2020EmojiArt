47baafb4ef34c163e64de9c81d02a27a29093d5a
jjonami <nickelcomplex@gmail.com>
2021-01-25 10:57:18 +0900

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by JJONAMI on 2021/01/25.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
